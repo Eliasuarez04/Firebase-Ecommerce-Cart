bb7c8ea982cf7e54b9844522c2f8ed5a3c64260f
Eliasuarez04 <125226499+Eliasuarez04@users.noreply.github.com>
2025-08-07 16:29:24 -0600

    Mejorado diseño de productos con imágenes reales

---
+++ src/data/products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    name: "Camiseta React",
+    price: 25,
+    image: "https://images.unsplash.com/photo-1571455786673-9d9d6c194f90?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Gorra JavaScript",
+    price: 15,
+    image: "https://images.unsplash.com/photo-1556306535-0f09a537f0a3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "Sticker Vue",
+    price: 5,
+    image: "https://plus.unsplash.com/premium_photo-1683120940374-e3f7465bbfa9?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    name: "Mousepad Dev",
+    price: 18,
+    image: "https://images.unsplash.com/photo-1671068514669-8134a08ca1a2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    name: "Taza de Café HTML",
+    price: 12,
+    image: "https://images.unsplash.com/photo-1586904118338-8ce35f4417dc?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    name: "Libreta CSS Pro",
+    price: 10,
+    image: "https://images.unsplash.com/photo-1623697899811-f2403f50685e?q=80&w=403&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+]
+
+export default products
